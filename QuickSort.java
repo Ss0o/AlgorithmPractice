b02d00a48ae1290d25f9ebe236dff40b0bae316f
Ss0o <sangsoo3510@gmail.com>
2025-04-27 18:12:22 +0900

    Quick Sort

--- QuickSort.java
+++ QuickSort.java
@@ -42,3 +42,12 @@ public class QuickSort {
         printArray(arr);
     }
 }
+
+/*
+    실행 흐름
+    quickSort함수에서 시작(좌측)값과 끝(우측)값을 오버로딩한 quickSort함수를 호출
+    오버로딩한 quickSort함수에서는 partition함수를 통해 전달받은 pivot값(part2)을 기준으로 좌측, 우측 부분집합으로 나누어
+    quickSort함수 재귀 호출
+    partition함수는 pivot값을 기준으로 좌, 우를 swap을 통해 정렬함 (pivot은 start와 end의 중간 값을 넣은 arr값)
+
+ */
