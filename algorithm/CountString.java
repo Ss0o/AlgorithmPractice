7c9a365bcc47c8fbe7e5e98b5fd2b0cfb92081d9
Ss0o <sangsoo3510@gmail.com>
2026-03-31 18:21:05 +0900

    String 예제2 (추가: 함수버전(startsWith사용, 중복 없이 세기))

--- algorithm/CountString.java
+++ algorithm/CountString.java
@@ -5,6 +5,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class CountString {
+    // 함수로 만들기 + startsWith 사용
+    public static int countSubString(String str, String target) {
+        int cnt = 0;
+        for (int i = 0; i <= str.length() - target.length(); i++) {
+            if(str.startsWith(target, i))
+                cnt++;
+        }
+        return cnt;
+    }
+    // 중복해서 안세기 ex) str: aaaa, target: aa 일 경우 cnt=2
+    public static int countSubStringNoDuplication(String str, String target) {
+        int cnt = 0;
+        for (int i = 0; i <= str.length() - target.length(); ) {
+            if(str.substring(i, i + target.length()).equals(target)) {
+                cnt++;
+                i+=target.length();
+            }
+            else
+                i++;
+        }
+        return cnt;
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();     // banana
@@ -17,5 +39,11 @@ public class CountString {
         }
 
         System.out.println("cnt = " + cnt);
+
+        str = "aaaa";
+        target = "aa";
+        System.out.println(countSubString(str,target));
+        System.out.println(countSubStringNoDuplication(str,target));
+
     }
 }
